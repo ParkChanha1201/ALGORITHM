3eac72005f297de1304856d819cacb8c356fb8b6
박찬하 <chanhacraft@naver.com>
2020-03-31 01:58:40 +0900

    bubblesort

--- src/sort/SortingApp.java
+++ src/sort/SortingApp.java
@@ -1,9 +1,40 @@
 package sort;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import sort.algorithm.BubbleSort;
 import sort.algorithm.SortingAlgorithm;
 
 public class SortingApp {
+	private static int LIST_SIZE = 30;
+	private static int MAX_INTEGER = 70;
+	
 	public static void main(String[] args) {
 		SortingAlgorithm algorithm;
+		algorithm = new BubbleSort();
+		
+		List<Integer> list = new ArrayList<Integer>();
+		
+		for(int i = 0; i < LIST_SIZE; i++) {
+			list.add((int)(Math.random() * MAX_INTEGER));
+		}
+		
+		System.out.println("정렬 전");
+
+		printList(list);
+		
+		algorithm.sort(list);
+		
+		System.out.println("정렬 후");
+		
+		printList(list);
+	}
+	
+	static void printList(List<Integer> list) {
+		for(int i = 0; i < list.size(); i++) {
+			System.out.print(list.get(i) + " ");
+		}
+		System.out.println();
 	}
 }
--- src/sort/algorithm/BubbleSort.java
+++ src/sort/algorithm/BubbleSort.java
@@ -3,10 +3,20 @@ package sort.algorithm;
 import java.util.List;
 
 public class BubbleSort implements SortingAlgorithm{
-
+	
 	@Override
 	public void sort(List<Integer> list) {
+		int length = list.size();
 		
+		for(int i = 0; i < length - 1; i++) {
+			for(int j = 0; j < length - 1 - i ; j++) {
+				if(list.get(j) > list.get(j + 1)) {
+					int tmp = list.get(j);
+					list.set(j, list.get(j + 1));
+					list.set(j + 1, tmp);
+				}
+			}
+		}
 	}
 
 }
