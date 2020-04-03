42df6dfe509bba36e6e438170aeefd1eecfe09c5
박찬하 <chanhacraft@naver.com>
2020-04-03 14:08:48 +0900

    create QuickSort.java file

---
+++ src/sort/algorithm/QuickSort.java
@@ -0,0 +1,17 @@
+package sort.algorithm;
+
+import java.util.List;
+
+public class QuickSort implements SortingAlgorithm{
+
+	@Override
+	public void sort(List<Integer> list) {
+		// TODO Auto-generated method stub
+		return;
+	}
+
+	@Override
+	public void printName() {
+		System.out.println("Quicksort");
+	}
+}
