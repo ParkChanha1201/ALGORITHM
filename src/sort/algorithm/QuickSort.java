dc78c1c8c190f7dc36e12ad37cc775bad9bb6c32
박찬하 <chanhacraft@naver.com>
2020-04-03 15:35:35 +0900

    feature-quicksort

--- src/sort/algorithm/QuickSort.java
+++ src/sort/algorithm/QuickSort.java
@@ -16,14 +16,52 @@ public class QuickSort implements SortingAlgorithm{
 		return instance;
 	}
 	
+	private int partition(List<Integer>list, int left, int right) {
+		//list.get(left) is pivot
+		int pivot = list.get(left);
+		//index for quick sort
+		int i = left + 1;
+		int j = right;
+		
+		while(i < j) {
+			while(list.get(i) < pivot) {
+				i++;
+			}
+			
+			while(list.get(j) > pivot) {
+				j--;
+			}
+
+			if(i < j) {
+				int temp = list.get(i);
+				list.set(i, list.get(j));
+				list.set(j, temp);
+			}
+		}
+		
+		list.set(left, list.get(j));
+		list.set(j, pivot);
+		
+		return j;
+	}
+	
+	private void quickSort(List<Integer>list, int left, int right){
+		if(left < right) {
+			int j = partition(list, left, right);
+			quickSort(list, left, j-1);
+			quickSort(list, j + 1, right);
+		}
+	}
+	
 	@Override
 	public void sort(List<Integer> list) {
-		// TODO Auto-generated method stub
-		return;
+		int length = list.size();
+		
+		quickSort(list, 0, length - 1);
 	}
 
 	@Override
 	public void printName() {
-		System.out.println("Quicksort(Unimplemented Algorithm)");
+		System.out.println("Quicksort");
 	}
 }
